b1d02772a65de096a71070b5714c1349625f1d68
Ananyahs <39176552+Ananyahs@users.noreply.github.com>
2023-08-12 22:20:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,51 @@
-/** @format */
+const searchBtn = document.getElementById('searchBtn');
+const apiKeyInput = document.getElementById('apiKey');
+const movieTitleInput = document.getElementById('movieTitle');
+const loader = document.querySelector('.loader');
+const resultsContainer = document.getElementById('movieResults');
 
-let arr = [
-  { id: 1, name: "john", age: "18", profession: "developer" },
-  { id: 2, name: "jack", age: "20", profession: "developer" },
-  { id: 3, name: "karen", age: "19", profession: "admin" },
-];
+searchBtn.addEventListener('click', searchMovies);
 
-function PrintDeveloperbyMap() {
-  //Write your code here , just console.log
+async function searchMovies() {
+    const apiKey = apiKeyInput.value;
+    const movieTitle = movieTitleInput.value;
+
+    if (!apiKey || !movieTitle) {
+        alert('Please enter API Key and Movie Title');
+        return;
+    }
+
+    loader.style.display = 'block';
+    resultsContainer.innerHTML = ''; // Clear previous results
+
+    try {
+        const response = await fetch(`https://www.omdbapi.com/?s=${movieTitle}&apikey=${apiKey}`);
+        const data = await response.json();
+
+        if (data.Error) {
+            alert(data.Error);
+        } else {
+            displayMovies(data.Search);
+        }
+    } catch (error) {
+        alert('An error occurred. Please try again later.');
+    }
+
+    loader.style.display = 'none';
 }
 
-function PrintDeveloperbyForEach() {
-  //Write your code here , just console.log
-}
+function displayMovies(movies) {
+    movies.forEach(movie => {
+        const card = document.createElement('div');
+        card.classList.add('card');
 
-function addData() {
-  //Write your code here, just console.log
-}
+        const poster = movie.Poster === 'N/A' ? 'no-poster.png' : movie.Poster;
 
-function removeAdmin() {
-  //Write your code here, just console.log
-}
+        card.innerHTML = `
+            <img src="${poster}" alt="${movie.Title}">
+            <h3>${movie.Title}</h3>
+        `;
 
-function concatenateArray() {
-  //Write your code here, just console.log
+        resultsContainer.appendChild(card);
+    });
 }
